0d2dc72f884d8594b0c04e12e0e2fa54063906ea
RonaldoChambilla <1520295@senati.pe>
2024-06-05 17:51:24 -0500

    actualizacion

--- 25.1-ajax.js
+++ 25.1-ajax.js
@@ -24,17 +24,24 @@ function buscarPacientes() {
 
     return;
 }
-function agregarFilas(id,paciente){
-    const html= 
+
+function agregarFilas(id,paciente) {
+    const html=
     "<tr>"+
-    "<td>"+paciente.nombres+"<td>"+
-    "<td>"+paciente.edad+"<td>"+
-    "<td>"+paciente.talla_m+"<td>"+
-    "<td>"+paciente.peso_kg+"<td>"+
-    "<td>"+paciente.sintoma_tos+"<td>"+
-    "<td>"+paciente.sintoma_fiebre+"<td>"+
-    "<td>"+paciente.sintoma_disnea+"<td>"+
-    "<td><button type = 'button'>Editar</><td>"+
-    "<tr>";
-    $(id + " tr:last").after(html);
+    "<td>"+paciente.nombres+"</td>"+
+    "<td>"+paciente.edad+"</td>"+    
+    "<td>"+paciente.talla_m+"</td>"+
+    "<td>"+paciente.peso_kg+"</td>"+
+    "<td>"+paciente.sintoma_tos+"</td>"+
+    "<td>"+paciente.sintoma_fiebre+"</td>"+
+    "<td>"+paciente.sintoma_disnea+"</td>"+
+    "<td><button type='button' onclick=editar('"+paciente.nombres+"','"+paciente.edad+"');>Editar</button></td>"+
+    "<td><button type='button' onclick=eliminar('"+paciente.nombres+"','"+paciente.edad+"');>eliminar</button></td>"+
+    "</tr>";
+    $(id+" tr:last").after(html);
+}
+ 
+function editar(nombres,edad) {
+    $('#exampleModal').modal('show');    
+    $("#nombre2").val(nombres);
 }
